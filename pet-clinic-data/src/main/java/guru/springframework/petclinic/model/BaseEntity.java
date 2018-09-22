3880a0325653ce81f54d65a9072d0c9e6892cd6c
Wiktor Rup <wiktor-rup@wp.pl>
2018-09-22 13:27:03 +0200

    refactoring to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
@@ -2,7 +2,7 @@ package guru.springframework.petclinic.model;
 
 import java.io.Serializable;
 
-public class BaseEntity implements Serializable{
+public class BaseEntity implements Serializable {
 
     private Long id;
 
