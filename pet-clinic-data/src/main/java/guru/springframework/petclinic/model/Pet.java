f7c49f6f4bbefab439d0fdd0518a7f473d121828
WRup <wiktor-rup@wp.pl>
2021-04-03 14:37:59 +0200

    Updated owners with JPA mappings. Closes #36

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
@@ -1,12 +1,26 @@
 package guru.springframework.petclinic.model;
 
+import org.hibernate.annotations.Cache;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petyType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
